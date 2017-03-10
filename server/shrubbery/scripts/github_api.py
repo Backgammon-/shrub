0638fde2c10eaca45993a649e46d433deb007537
Mike Guo <j.nwgixv@gmail.com>
2017-03-10 17:27:52 -0500

    Add skeletons for Github API request methods.

---
+++ server/shrubbery/scripts/github_api.py
@@ -0,0 +1,23 @@
+""" Functions relating to accessing the github API. """
+
+class GithubAPI:
+    def get_key(secret):
+        """
+            Given the user's Github username/password and a new secret, get a
+            new OAuth token.
+        """
+        # https://developer.github.com/v3/oauth_authorizations/#create-a-new-authorization
+        pass;
+
+    def get_repos():
+        """
+            List accessible repositories for the authenticated user.
+        """
+        # https://developer.github.com/v3/repos://developer.github.com/v3/repos/
+        pass;
+
+    def get_repo_issues():
+        # Remember to check the has_issues bool on the repo
+        pass;
+
+    # def get_issue_comments, etc.
