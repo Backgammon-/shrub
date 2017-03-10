fa3fc9ca145a91feed14ba6657a1811c16d3081e
Nicholas Kachur <nick.e.kachur@gmail.com>
2017-03-10 17:57:52 -0500

    continue working on validate command

--- client/shrub/scripts/cli.py
+++ client/shrub/scripts/cli.py
@@ -1,10 +1,13 @@
-"""The Shrub client application."""
+"""The Shrub command-line interface."""
 
-from getpass import getuser
+from getpass import getuser, getpass
 from os.path import expanduser
 
+
 import click
-from paramiko import SSHClient, WarningPolicy
+from paramiko import (AuthenticationException, PasswordRequiredException,
+                      SSHClient, WarningPolicy)
+
 
 CONNECTION_STRING = 'shrub@104.236.0.123'
 
@@ -17,6 +20,7 @@ def cli():
 @click.argument('username')
 @click.option('--key-path', default=expanduser('~/.ssh/id_rsa'),
               type=click.Path(exists=True))
+@click.option('--insecure', is_flag=True)
 @click.command()
 def register(username, key_path):
     """Register a new GitHub user with the Shrub server."""
@@ -27,6 +31,7 @@ def register(username, key_path):
 @click.argument('connection_string', default=CONNECTION_STRING)
 @click.option('--key-path', default=expanduser('~/.ssh/id_rsa'),
               type=click.Path(exists=True))
+@click.option('--insecure', is_flag=True)
 @click.command()
 def validate(connection_string, key_path):
     """
@@ -50,12 +55,32 @@ def validate(connection_string, key_path):
 
     client = SSHClient()
     client.set_missing_host_key_policy(WarningPolicy())
-    client.connect(username=username, hostname=servername,
-                   key_filename=key_path)
+    try:
+        client.connect(username=username, hostname=servername,
+                       key_filename=key_path)
+    except PasswordRequiredException:
+        prompt = 'Enter the password for key {}: '.format(key_path)
+        password = getpass(prompt=prompt)
+    try:
+        client.connect(username=username, password=password,
+                       hostname=servername, key_filename=key_path)
+    except AuthenticationException:
+        click.echo('Wrong password, exiting.')
+
     stdin, stdout, stderr = client.exec_command('ls -l')
+
+    stdin, stdout, stderr = client.exec_command('shrubbery register
+            {}'.format(username))
+
+    output = stdout.read()
+    client.close()
+
     if stdin or stdout or stderr:
         click.echo('Successfully connected, but Shrub does not give '
                    'interactive access.')
+
+        # TODO: This is temp, remove soon.
+        click.echo('P.S. output was\n\n: {}'.format(output))
         return 0
     else:
         click.echo('Error: could not connect')
@@ -63,6 +88,9 @@ def validate(connection_string, key_path):
 
     return 1
 
+
+def create_client(username, hostname, keyname):
+    """Utility function to return an open SSHClient instance."""
 
 cli.add_command(register)
 cli.add_command(validate)
