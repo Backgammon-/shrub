20a8f40fef327c6ebd6f85bbbeb3153d56a4397d
Nicholas Kachur <nick.e.kachur@gmail.com>
2017-03-12 10:26:02 +0000

    add initial server/client cli communication

--- client/shrub/scripts/cli.py
+++ client/shrub/scripts/cli.py
@@ -3,16 +3,17 @@
 
 from getpass import getuser, getpass
 from os.path import expanduser
-
+from sys import exit
 
 import click
-from paramiko import (AuthenticationException, PasswordRequiredException,
+from paramiko import (AuthenticationException, PasswordRequiredException, RSAKey,
                       SSHClient, WarningPolicy)
 
 
 CONNECTION_STRING = 'shrub@104.236.0.123'
 
 
+##### MAIN GROUP #######################################################
 @click.group()
 def cli():
     pass
@@ -26,19 +27,59 @@ def cli():
 @click.command()
 def register(username, key_path, insecure):
     """Register a new GitHub user with the Shrub server."""
+
+### Temporary section for telling the user what _should_ happen.
     if insecure:
         click.echo('Warning: using insecure code paths.')
+
+    fingerprint = get_key_fingerprint(key_path)
     click.echo("This command should register a user with username '{}' "
-               "and ssh key '{}'".format(username, key_path))
+               "and ssh key '{} using fingerprint '{}''".format(
+                   username, key_path, fingerprint))
+
+### Section for setting up the sever command string.
+# This is a ripe place to open up user input vulnerabilities.
+    options_string = ''
+    if insecure:
+        options_string += ' --insecure'
+    command_string = 'shrubbery register {} {}'.format(fingerprint, username)
+    command_string += options_string
+
+### Section for opening up SSH connection.
+    username, hostname = get_connection_tuple(CONNECTION_STRING)
+    if not (username and hostname):
+        exit('Bad connection string')
+    client = open_ssh_client(username, hostname, key_path)
+
+### Execute the command, and perform any back and forth logic necessary.
+# Interactivity could be implemented at this point if necessary using
+# by using multiple exec_command calls and asking for user input where
+# necessary.
+#
+# stdin, stdout, and stderr are file-like objects, but are only
+# readable while the client is open
+    stdin, stdout, stderr = client.exec_command(command_string)
+
+### Parse server output here.
+    print('\n\nServer stdout:')
+    for line in stdout:
+        print(line)
+    print('\n\nServer stderr:')
+    for line in stderr:
+        print(line)
+
+### Close the connection.
+# Make sure to do this, else Python might hang and leave your shell
+# frozen.
+    client.close()
 
 
 ##### VALIDATE #########################################################
-@click.argument('connection_string', default=CONNECTION_STRING)
 @click.option('--key-path', default=expanduser('~/.ssh/id_rsa'),
               type=click.Path(exists=True))
 @click.option('--insecure', is_flag=True)
 @click.command()
-def validate(connection_string, key_path, insecure):
+def validate(key_path, insecure):
     """
         Attempt to connect to a Shrub server and print a message
         detailing success or failure.
@@ -47,34 +88,13 @@ def validate(connection_string, key_path, insecure):
         click.echo('Warning: using insecure code paths.')
 
     click.echo("Attempting to connect to '{}' with key '{}'".format(
-               connection_string, key_path))
+               CONNECTION_STRING, key_path))
 
-    connection_array = connection_string.split('@')
-    if len(connection_array) == 2:
-        username = connection_array[0]
-        servername = connection_array[1]
-    elif len(connection_array) == 1:
-        username = getuser()
-        servername = connection_array[0]
-    else:
-        click.echo("Error: Connection string should be in the format "
-                   "'user@server'")
-        return 1
+    username, hostname = get_connection_tuple(CONNECTION_STRING)
+    if not (username and hostname):
+        exit('Bad connection string')
 
-    client = SSHClient()
-    client.set_missing_host_key_policy(WarningPolicy())
-    try:
-        client.connect(username=username, hostname=servername,
-                       key_filename=key_path)
-    except PasswordRequiredException:
-        prompt = 'Enter the password for key {}: '.format(key_path)
-        password = getpass(prompt=prompt)
-    try:
-        client.connect(username=username, password=password,
-                       hostname=servername, key_filename=key_path)
-    except AuthenticationException:
-        click.echo('Wrong password, exiting.')
-
+    client = open_ssh_client(username, hostname, key_path)
     stdin, stdout, stderr = client.exec_command('ls -l')
     output = stdout.read()
     client.close()
@@ -82,12 +102,10 @@ def validate(connection_string, key_path, insecure):
     if stdin or stdout or stderr:
         click.echo('Successfully connected, but Shrub does not give '
                    'interactive access.')
-        return 0
+        exit(0)
     else:
-        click.echo('Error: could not connect')
-        return 1
-
-    return 1
+        exit('Failed to connect')
+    exit(1)
 
 
 ##### SHOW GROUP #######################################################
@@ -132,6 +150,56 @@ def issue():
 def comment():
     """Create a comment on an issue or pull request."""
     click.echo('Creating a comment.')
+
+
+##### Utils ############################################################
+def get_connection_tuple(connection_string):
+    """
+        Return the username and hostname from an arbitrary connection
+        string. Returns None if the supplied string is invalid.
+    """
+    connection_array = connection_string.split('@')
+    if len(connection_array) == 2:
+        username = connection_array[0]
+        servername = connection_array[1]
+    elif len(connection_array) == 1:
+        username = getuser()
+        servername = connection_array[0]
+    else:
+        return (None, None)
+    return (username, servername)
+
+
+def open_ssh_client(username, servername, keypath):
+    """Return an open paramiko.SSHClient instance."""
+    client = SSHClient()
+    client.set_missing_host_key_policy(WarningPolicy())
+    try:
+        client.connect(username=username, hostname=servername,
+                       key_filename=keypath)
+    except PasswordRequiredException:
+        prompt = 'Enter the password for key {}: '.format(keypath)
+        password = getpass(prompt=prompt)
+        try:
+            client.connect(username=username, password=password,
+                           hostname=servername, key_filename=keypath)
+        except AuthenticationException:
+            exit('Failed to authenticate')
+    return client
+
+
+def get_key_fingerprint(keypath):
+    """Return an SSH key fingerprint from its path."""
+    try:
+        key = RSAKey.from_private_key_file(keypath)
+    except PasswordRequiredException:
+        prompt = 'Enter the password for key {}: '.format(keypath)
+        password = getpass(prompt=prompt)
+        try:
+            key = RSAKey.from_private_key_file(keypath, password=password)
+        except AuthenticationException:
+            exit('Failed to authenticate')
+    return key.get_base64()
 
 
 ##### SETUP GROUPS #####################################################
--- server/shrubbery/scripts/cli.py
+++ server/shrubbery/scripts/cli.py
@@ -8,12 +8,17 @@ def cli():
     pass
 
 
+##### REGISTER #########################################################
+@click.option('--insecure', is_flag=True, help='Use insecure code paths')
 @click.argument('username')
+@click.argument('fingerprint')
 @click.command()
-def register(username):
+def register(username, fingerprint, insecure):
     """Register a new GitHub user with the Shrub server."""
+    if insecure:
+        click.echo('Warning: using insecure code paths.')
     click.echo("This command should register a user with username '{}' "
-               "and ssh key '{}'".format(username))
+               "and fingerprint '{}'".format(username, fingerprint))
 
 
 cli.add_command(register)
