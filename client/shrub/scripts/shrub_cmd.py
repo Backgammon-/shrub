d99d17c1467aeca1a23c5e765dae3ade075270d5
M. Guo <j.nwgixv@gmail.com>
2017-03-12 18:57:09 -0400

    Define additional interactive commands; prepare to implement send_server_cmd

--- client/shrub/scripts/shrub_cmd.py
+++ client/shrub/scripts/shrub_cmd.py
@@ -3,10 +3,22 @@ import getpass
 
 class Shrub(cmd.Cmd):
     prompt = "shrub> "
-    intro = "Welcome to Shrub!"
+    intro = """Welcome to shrub!\nTo get started, try "help".\n"""
 
     doc_header = "Available commands:"
     ruler = '-'
+
+    ##### OVERRIDES #####
+    def emptyline(self):
+        # Follow shell behavior; do nothing and just print a new prompt.
+        pass
+
+    def default(self, line):
+        print("""shrub: {}: command not found. Try "help".""".format(line.split(' ', 1)[0]))
+
+    ##### COMMANDS #####
+    def do_register(self, line):
+        print("TODO: register")
 
     def do_login(self, line):
         """login [username]
@@ -16,8 +28,17 @@ class Shrub(cmd.Cmd):
         pass
 
     def do_EOF(self, line):
+        """Send EOF (Ctrl-D) to exit."""
         print("\nBye!")
         return True
+
+    def do_show_issues(self, line):
+        print("TODO: show issues")
+
+    ##### HELPERS #####
+    def send_server_cmd():
+        # TODO: Use Paramiko to execute a command on the server and return the server's response.
+        pass
 
 if __name__ == '__main__':
     Shrub().cmdloop()
