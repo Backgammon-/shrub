a7eb930a719359cd4ef1f491264a722795d8bce1
Maksim Bazhydlouski <mb3497@drexel.edu>
2017-03-12 21:05:53 -0400

    Placeholder for validate.py

---
+++ client/shrub/scripts/validate.py
@@ -0,0 +1,8 @@
+import sys
+
+
+
+def validate(line)
+    tokens = line.split(' ')
+    if (tokens[0] == 'list' || tokens == 'show' || 'create' || 'update' || 'edit' || 'comment' || 'delete')
+	return True
